e68095c1bf98d35269ba6c2c765512e441ea2a12
dheeraj <dheerajjha834@gmail.com>
2023-05-25 01:03:22 +0530

    age calculator

---
+++ App.js
@@ -0,0 +1,31 @@
+const calculate = () => {
+    setInterval(() => {
+        //input Birth Date
+        let birthdate = new Date(document.getElementById('birthdate').value);
+        console.log(birthdate.value)
+        let now = new Date();
+        let ageInMs = now.getTime() - birthdate.getTime();
+
+
+        let ageInS = ageInMs / 1000; //to Seconds
+        let ageInMins = ageInS / 60; //to Minuts
+        let ageInHrs = ageInMins / 60; //to Hours
+        let ageInDays = ageInHrs / 24; //to days
+        let ageInMonths = ageInDays / 30.4375; //to months
+        let ageInYears = ageInMonths / 12; //to years
+
+
+
+
+        document.querySelector('#years').innerHTML = Math.floor(ageInYears);
+        document.querySelector('#months').innerHTML = Math.floor(ageInMonths % 12);
+        document.querySelector('#days').innerHTML = Math.floor(ageInDays % 30.4375);
+        document.querySelector('#hours').innerHTML = Math.floor(ageInHrs % 24);
+        document.querySelector('#minutes').innerHTML = Math.floor(ageInMins % 60);
+        document.querySelector('#seconds').innerHTML = Math.floor(ageInS % 60);
+        document.querySelector('#seconds').style.borderBottom = '1px grey solid';
+    }, 1000)
+}
+const reset = () => {
+    window.location.reload()
+}
